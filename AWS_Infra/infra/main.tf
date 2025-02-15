8f4264eba19fbfdd541585cb30d82c52bcc4e785
Stanley <stanleyjoshua.m@gmail.com>
2025-02-15 18:15:57 +0530

    changes in module path

--- AWS_Infra/infra/main.tf
+++ AWS_Infra/infra/main.tf
@@ -1,9 +1,9 @@
 module "vpc" {
-  source = "./modules/vpc"
+  source = "../modules/vpc"
 }
 
 module "ec2" {
-  source            = "./modules/ec2"
+  source            = "../modules/ec2"
   ami_id            = data.aws_ami.linux.id 
   instance_type     = "t2.micro"
   public_subnet_id  = module.vpc.public_subnet_id
