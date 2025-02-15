0d071a0e903bdc7dbf3a9708a3e9399d36434d96
stan1417 <106138919+stan1417@users.noreply.github.com>
2025-02-15 17:27:03 +0530

    Update main.tf

--- AWS/infra/main.tf
+++ AWS/infra/main.tf
@@ -4,7 +4,7 @@ module "vpc" {
 
 module "ec2" {
   source            = "./modules/ec2"
-  ami_id            = "ami-12345678"  
+  ami_id            = data.aws_ami.linux.id 
   instance_type     = "t2.micro"
   public_subnet_id  = module.vpc.public_subnet_id
   private_subnet_id = module.vpc.private_subnet_id
